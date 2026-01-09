964f2b094cb1da17d28b3bc511ad0786e364bb7e
Lakshya Kapoor <kapoorlakshya42@gmail.com>
2026-01-10 00:43:32 +0530

    fix(context): close getAllDoctors function; fix parsing issue in AdminContext

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,12 +1,28 @@
 import { createContext, useEffect, useState } from 'react';
 import axios from 'axios'
+import { toast } from 'react-toastify';
 
 export const AdminContext = createContext({});
 
 const AdminContextProvider = (props) => {
     const [atoken, setAtoken] = useState(localStorage.getItem('atoken') ? localStorage.getItem('atoken') : '');
-
+    const [doctors, setDoctors] = useState([]);
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const getAllDoctors = async () => {
+        try {
+            const {data} = await axios.post(backendUrl + '/api/admin/all-doctors', {}, {headers: {atoken}});
+            if(data.success){
+                setDoctors(data.doctors);
+                console.log(data.doctors);
+                
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
 
     // ensure axios baseURL is set from env
     useEffect(() => {
@@ -29,6 +45,8 @@ const AdminContextProvider = (props) => {
         atoken,
         setAtoken,
         backendUrl,
+        doctors,
+        getAllDoctors
     }
 
     return (
--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useContext } from 'react';
+import { AdminContext } from '../../context/AdminContext';
+import { useEffect } from 'react';
 
 const DoctorsList = () => {
+
+  const { doctors, aToken, getAllDoctors } = useContext(AdminContext);
+
+  useEffect(() => {
+    if (aToken) {
+      getAllDoctors();
+    }
+  }, [aToken]);
+
   return (
-    <div>DoctorsList</div>
+    <div>
+      <h1>All Doctors</h1>
+      <div>
+        {doctors.map((item, index) => (
+          <div key={index}>
+            <img src={item.image} alt={item.name} />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
